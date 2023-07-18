caf9b17ec76e7603aa7a55bcc52f38e2c8058ffd
Rodrigo Quisbert <seintecarica@gmail.com>
2023-07-18 01:40:21 -0400

    Creando algunas relaciones entre tablas

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -4,14 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //el id sea autoincrementable
     private Integer id;
     private String nombre;
     private String descripcion;
     private String imagen;
     private Double precio;
     private int cantidad;
+
+    @ManyToOne
+    private Usuario usuario;
 }
--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -2,10 +2,18 @@ package com.curso.ecommerce.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.loader.plan.spi.BidirectionalEntityReference;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "usuarios") // Para modificar el nombre de la tabla en la BBDD.
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //el id sea autoincrementable
     private Integer id;
     private String nombre;
     private String username;
@@ -15,4 +23,6 @@ public class Usuario {
     private String tipo;
     private String password;
 
+    @OneToMany(mappedBy = "usuario") //En la tabla Producto existe un atributo llamado usuario;
+    private List<Producto> producto;
 }
