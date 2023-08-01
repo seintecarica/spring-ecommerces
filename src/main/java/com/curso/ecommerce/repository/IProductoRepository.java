d1fc4789fd55c2731975d44d8e94c8a4c6097eb9
Rodrigo Quisbert <seintecarica@gmail.com>
2023-08-01 19:16:07 -0400

    Crear el repository, las clases de servicio y el método de ProductoController

---
+++ src/main/java/com/curso/ecommerce/repository/IProductoRepository.java
@@ -0,0 +1,10 @@
+package com.curso.ecommerce.repository;
+
+import com.curso.ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
