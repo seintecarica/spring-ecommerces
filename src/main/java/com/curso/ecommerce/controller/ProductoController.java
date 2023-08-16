98c5298f234507785affb6642381728fa00f670e
Rodrigo Quisbert <seintecarica@gmail.com>
2023-08-16 19:38:35 -0300

    Funcionalidad guardar producto, vista create y logger para la clase ProductoController

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,14 +1,41 @@
 package com.curso.ecommerce.controller;
 
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+    @Autowired //Para que spring lo instancie
+    private ProductoService productoService;
+
     @GetMapping("")
     public String show(){
         return "productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este es el objeto producto: {}", producto);
+
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+        producto.setUsuario(u);
+        this.productoService.save(producto);
+        return "redirect:/productos"; //Va a cargar la vista show
     }
 }
--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,13 +1,14 @@
 package com.curso.ecommerce.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.loader.plan.spi.BidirectionalEntityReference;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "usuarios") // Para modificar el nombre de la tabla en la BBDD.
@@ -28,4 +29,15 @@ public class Usuario {
 
     @OneToMany(mappedBy = "usuario")
     private List<Orden> ordenes;
+
+    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.password = password;
+    }
 }
