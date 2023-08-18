e5f93797adbbc648546daacc025ba250f2d4602c
Rodrigo Quisbert <seintecarica@gmail.com>
2023-08-18 00:57:51 -0300

    Mostrar la vista del producto al usuario

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,21 +1,32 @@
 package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductoService productoService;
     @GetMapping("/")
     public String home(Model model){
         model.addAttribute("productos", this.productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        this.log.info("Id producto enviado como parámetro {}", id);
+        return "/usuario/productohome";
     }
 }
