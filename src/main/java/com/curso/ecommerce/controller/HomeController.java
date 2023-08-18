895cdb5e00c0169c24bf9e51eca7848c415c382a
Rodrigo Quisbert <seintecarica@gmail.com>
2023-08-18 02:14:23 -0300

    Obtención de la cant y búsq del producto en el homeController
    Para añadir al carrito de compra

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import com.curso.ecommerce.model.DetalleOrden;
+import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.ProductoService;
 import org.slf4j.Logger;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -18,6 +22,11 @@ public class HomeController {
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductoService productoService;
+
+    //Para almacenar los detalles de la orden
+    private List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+    //Datos de la orden
+    private Orden orden = new Orden();
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("productos", this.productoService.findAll());
@@ -35,7 +44,14 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+        Optional<Producto> optionalProducto = this.productoService.get(id);
+        //this.log.info("Producto añadido: {}", optionalProducto.get());
+        this.log.info("Cantidad: {}", cantidad);
+
         return "usuario/carrito";
     }
 }
