1e1b8d00130e4b85ad2539bc996117b4b389cc83
Rodrigo Quisbert <seintecarica@gmail.com>
2023-09-27 02:03:46 -0300

    Se ha creado los repositorios y clases de Orden y Detalle

---
+++ src/main/java/com/curso/ecommerce/service/DetalleOrdenServiceImpl.java
@@ -0,0 +1,16 @@
+package com.curso.ecommerce.service;
+
+import com.curso.ecommerce.model.DetalleOrden;
+import com.curso.ecommerce.repository.IDetalleOrdenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DetalleOrdenServiceImpl implements IDetalleOrdenService{
+    @Autowired
+    private IDetalleOrdenRepository detalleOrdenRepository;
+    @Override
+    public DetalleOrden save(DetalleOrden detalleOrden) {
+        return this.detalleOrdenRepository.save(detalleOrden);
+    }
+}
